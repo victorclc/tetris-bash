665dad07230bef56143320fd474af0a4aca8484d
Victor Corte <victorcortelc@gmail.com>
2023-04-24 10:46:35 -0300

    tetrominos representation

--- tetris.sh
+++ tetris.sh
@@ -1,13 +1,35 @@
 #!/usr/bin/env bash
 
+# BOARD CONFIGURATION
+
 LINES=20
 COLUMNS=10
 TICK_DELAY=1
 
-CMD_DOWN=0
+# BINDS CONFIGURATION
+
+CMD_DOWN='s'
+
+tetrominos=(
+    "0010001000100010" # I-Block
+    "0100011000100000" # S-block
+    "0010011001000000" # Z-block
+    "0110011000000000" # O-block
+    "0110010001000000" # J block
+    "0110001000100000" # L-block
+    "0010001100100000" # T-Block
+)
+
+# GAME STATE VARIABLES
 
 declare -a lines_state
+current_piece_index=$((RANDOM % 7))
+current_piece_rotation=0 # 0 = 0 degrees, 1 = 90 degrees, 2 = 180 degrees, 3 = 270 degrees
+current_piece_pos_y=0
+current_piece_pos_x=0
+
 j=0
+
 
 function init() {
     for i in {0..20}; do
@@ -40,6 +62,8 @@ function controller() {
 
 function draw_board() {
     clear
+    declare -a board_view
+
     for line in "${lines_state[@]}"; do
         line=$(echo -e ${line//0/'. '})
         line=$(echo -e ${line//1/'[]'})
